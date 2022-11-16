11038ab97b47aa41100dea0f5e51d5f77b617bf4
Joshua Crook <joshuacrook400@gmail.com>
2022-11-16 15:32:01 +1000

    Working on task 2

--- src/ML.jl
+++ src/ML.jl
@@ -1,11 +1,42 @@
 using Flux
 using Flux: binarycrossentropy #Loss function
 using Flux: params #Used for automatic differenation (but in Project replace auto-diff with explicit gradient)
+using FLUX: onehotbatch
 using Random
+using LinearAlgebra
 
 
 
 Random.seed!(0)
+
+
+"""
+Returns the accuracy of a linear training model
+"""
+function train_linear_one_vs_all(train_imgs, train_labels)
+    n_train = length(train_imgs)
+
+    X = vcat([vec(train_imgs[:,:,k])' for k in 1:n_train]...);
+
+    A = [ones(n_train) X];
+    Adag = pinv(A);
+   
+    tfPM(x) = x ? +1 : -1
+    yDat(k) = tfPM.(onehotbatch(train_labels_MINST,0:9)'[:,k+1])
+    betas = [Adag*yDat(k) for k in 0:9]; #this is the trained model (a list of 10 beta coeff vectors)
+
+    linear_classify(square_image) = argmax([([1 ; vec(square_image)])'*bets[k] for k in 1:10])-1
+    predictions = [linear_classify(test_imgs[:,:,k]) for k in 1:n_test]
+    confusionMatrix = [sum((predictions .== i) .& (test_labels .== j)) for i in 0:9, j in 0:9]
+
+
+    return confusionMatrix
+end
+
+
+
+
+
 
 #functions
 sig(x) = 1/(1+float(MathConstants.e)^-x)
@@ -34,11 +65,15 @@ function train_logistic(train_data, train_labels, data_size ;num_epochs = 20, mi
         #Loop over mini-batches in epoch
         start_time = time_ns()
         for batch in Iterators.partition(1:data_size, mini_batch_size)
+            
             gs = gradient(()->loss(train_data'[:,batch], train_labels[batch]'), params(w, b))
+        
             b = b - η*gs[b]
             w = w - η*gs[w]
+            
         end
         end_time = time_ns()
+        
 
         #record/display progress
         loss_value = loss(train_data', train_labels')
